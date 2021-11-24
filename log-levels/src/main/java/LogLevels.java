4b6eaf07da939fa692bff86a62335ce96f0bce83
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-24 14:42:12 +0300

    log levels solution

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,14 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        return logLine.replaceAll("\\[.*\\]: ", "").strip();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        return logLine.substring(logLine.indexOf('[') + 1, logLine.indexOf(']')).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }
