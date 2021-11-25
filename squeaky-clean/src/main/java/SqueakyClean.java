941813bb3a28e1f6b7eb472f947c54cf2d7ae834
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-25 15:27:14 +0300

    squeaky clean small refactoring

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -3,13 +3,12 @@ class SqueakyClean {
         String partiallyCleaned = identifier.replaceAll(" ", "_")
                .replaceAll("\\\0", "CTRL")
                .replaceAll("[α-ω, 0-9]", "");
-        StringBuilder b = new StringBuilder();
 
+        StringBuilder b = new StringBuilder();
         for (int i = 0; i < partiallyCleaned.length(); i++){
             Character c = partiallyCleaned.charAt(i);
             if (c.equals('-')) {
-                c = partiallyCleaned.charAt(++i);
-                b.append(Character.toUpperCase(c));
+                b.append(Character.toUpperCase(partiallyCleaned.charAt(++i)));
                 continue;
             }
             if (!Character.isLetterOrDigit(c) && !c.equals('_')) continue;
