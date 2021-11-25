8a9de06c1651ea0337d96b34d49d74f50fe000ec
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-25 15:22:26 +0300

    squeaky clean solution

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,20 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        String partiallyCleaned = identifier.replaceAll(" ", "_")
+               .replaceAll("\\\0", "CTRL")
+               .replaceAll("[α-ω, 0-9]", "");
+        StringBuilder b = new StringBuilder();
+
+        for (int i = 0; i < partiallyCleaned.length(); i++){
+            Character c = partiallyCleaned.charAt(i);
+            if (c.equals('-')) {
+                c = partiallyCleaned.charAt(++i);
+                b.append(Character.toUpperCase(c));
+                continue;
+            }
+            if (!Character.isLetterOrDigit(c) && !c.equals('_')) continue;
+            b.append(c);
+        }
+        return b.toString();
     }
 }
