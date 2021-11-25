db63f9a844de7727de56c3894b0bb84f82969431
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-25 15:46:23 +0300

    elons toy car solution

--- elons-toy-car/src/main/java/ElonsToyCar.java
+++ elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,17 +1,25 @@
 public class ElonsToyCar {
+
+    private int distance = 0;
+    private int charge = 100;
+
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.buy()  method");
+        return new ElonsToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.distanceDisplay()  method");
+        return "Driven " + distance + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+        if (charge == 0) return "Battery empty";
+        return "Battery at " + charge + "%";
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
+        if (charge > 0) {
+            distance += 20;
+            charge -= 1;
+        }
     }
 }
