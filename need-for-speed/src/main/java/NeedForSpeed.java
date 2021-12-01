617921ff9b1d3bb44e1ba4d26900a0191c1c95f1
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-01 10:13:38 +0300

    need for speed solution

--- need-for-speed/src/main/java/NeedForSpeed.java
+++ need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,27 +1,47 @@
 class NeedForSpeed {
     // TODO: define the constructor for the 'NeedForSpeed' class
+    private int charge  = 100;
+    private int distanceDriven = 0;
+    private final int drain;
+    private final int speed;
+
+    public NeedForSpeed(int speed, int drain) {
+        this.speed = speed;
+        this.drain = drain;
+    }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return charge <= 0;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return distanceDriven;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (!batteryDrained()) {
+            charge -= drain;
+            distanceDriven += speed;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
     // TODO: define the constructor for the 'RaceTrack' class
+    private final int length;
+
+    public RaceTrack(int length) {
+        this.length = length;
+    }
 
     public boolean carCanFinish(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.carCanFinish() method");
+        while(!car.batteryDrained()) {
+            car.drive();
+        }
+        return car.distanceDriven() >= length;
     }
 }
