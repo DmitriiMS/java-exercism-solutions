f8cc39349e3494ee48b2c893909f60f5a367614c
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-04 20:58:15 +0300

    salary calculator solution

--- salary-calculator/src/main/java/SalaryCalculator.java
+++ salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,18 @@
 public class SalaryCalculator {
     public double multiplierPerDaysSkipped(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerDaysSkipped() method");
+        return daysSkipped <= 5 ? 1 : 0.85;
     }
 
     public int multiplierPerProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerProductsSold() method");
+        return productsSold <= 20 ? 10 : 13;
     }
 
     public double bonusForProductSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductSold() method");
+        return (double) productsSold * multiplierPerProductsSold(productsSold);
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
-    } 
+        double salary = (1000 * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold));
+        return salary > 2000 ? 2000 : salary;
+    }
 }
