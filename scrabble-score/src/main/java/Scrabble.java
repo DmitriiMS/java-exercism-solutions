9d733458d8b6d50bebe120cd893ae00ca5756c21
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-16 10:53:47 +0300

    scrabble score solution with IntStream via the string.chars() method

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,14 +1,10 @@
-import java.util.Map;
-
 class Scrabble {
 
     private final static int[] LETTER_SCORES = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-    private int score = 0;
+    private final int score;
 
     Scrabble(String word) {
-       for (char c : word.toUpperCase().toCharArray()) {
-           score += LETTER_SCORES[c - 'A'];
-       }
+       score = word.toUpperCase().chars().map(i -> LETTER_SCORES[i - 'A']).sum();
     }
 
     int getScore() {
