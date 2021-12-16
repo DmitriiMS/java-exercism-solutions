649a6d2f8e673f381b4ca470073d7aa04b4e48e6
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-16 10:49:42 +0300

    scrabble score solution

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,18 @@
+import java.util.Map;
+
 class Scrabble {
 
+    private final static int[] LETTER_SCORES = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    private int score = 0;
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+       for (char c : word.toUpperCase().toCharArray()) {
+           score += LETTER_SCORES[c - 'A'];
+       }
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return score;
     }
 
 }
