4d626aa63a18e1c397c28c43b6366b4147362610
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-16 13:07:02 +0300

    secret handshake solution

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Signal> shake = new ArrayList<Signal>();
+        String binaryNum = new StringBuilder(Integer.toBinaryString(number)).reverse().toString();
+        for (int i = 0; i < binaryNum.length(); i++) {
+            if (binaryNum.charAt(i) == '1' && i < 4) {
+                shake.add(Signal.values()[i]);
+            }
+        }
+        if (binaryNum.length() > 4 && binaryNum.charAt(4) == '1') { Collections.reverse(shake); }
+        return shake;
     }
 
 }
