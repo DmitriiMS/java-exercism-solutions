e44990eea5b59c5b74e56c0bb9b89ddbef3ac6c0
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-12-18 19:29:20 +0300

    handshake calculator small refactoring

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 class HandshakeCalculator {
@@ -8,8 +11,8 @@ class HandshakeCalculator {
     List<Signal> calculateHandshake(int number) {
         List<Signal> shake = new ArrayList<Signal>();
         String binaryNum = new StringBuilder(Integer.toBinaryString(number)).reverse().toString();
-        for (int i = 0; i < binaryNum.length(); i++) {
-            if (binaryNum.charAt(i) == '1' && i < 4) {
+        for (int i = 0; i < binaryNum.length() && i < 4; i++) {
+            if (binaryNum.charAt(i) == '1') {
                 shake.add(Signal.values()[i]);
             }
         }
