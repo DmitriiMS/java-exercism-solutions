2c43760a485e8f3aa1457e6b65734b0086245c95
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 16:51:53 +0300

    resistor-color solution

--- resistor-color/src/main/java/ResistorColor.java
+++ resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,20 @@
 class ResistorColor {
+    private final String[] colors;
+
+    public ResistorColor() {
+        colors = colors();
+    }
+
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for(int i = 0; i < colors.length; i++) {
+            if(colors[i].equals(color)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("no such color in collection: " + color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     }
 }
