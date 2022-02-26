7465901faa5566e6cfe93141f5ced09debfd86ae
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 16:32:35 +0300

    grains solution

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -1,13 +1,30 @@
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Grains {
-
-    BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public static void main(String[] args) {
+        long[] times = new long[1000000];
+        long start, end;
+        for(int i = 0; i < 1000000; i++){
+            start  = System.nanoTime();
+            grainsOnBoard();
+            end = System.nanoTime();
+            times[i] = end - start;
+        }
+        System.out.println(Arrays.stream(times).average());
     }
 
-    BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    static BigInteger grainsOnSquare(final int square) {
+        if(square <= 0 || square > 64){
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return new BigInteger("2").pow(square-1);
     }
 
+    static BigInteger grainsOnBoard() {
+        return IntStream.range(1,65)
+                .mapToObj(Grains::grainsOnSquare)
+                .reduce(BigInteger.valueOf(0),BigInteger::add);
+    }
 }
