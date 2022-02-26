52c795fb438c69e2995e52d5b4df5c1d5c3a6aae
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 14:15:37 +0300

    raindrops cleanup

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -2,18 +2,9 @@ class RaindropConverter {
 
     String convert(int number) {
         String result = "";
-        if (number % 3 == 0) {
-            result = result.concat("Pling");
-        }
-        if (number % 5 == 0) {
-            result = result.concat("Plang");
-        }
-        if (number % 7 == 0) {
-            result = result.concat("Plong");
-        }
-        if (result.isEmpty()) {
-            return Integer.toString(number);
-        }
-        return result;
+        if (number % 3 == 0) { result = result.concat("Pling"); }
+        if (number % 5 == 0) { result = result.concat("Plang"); }
+        if (number % 7 == 0) { result = result.concat("Plong"); }
+        return result.isEmpty() ? Integer.toString(number) : result;
     }
 }
