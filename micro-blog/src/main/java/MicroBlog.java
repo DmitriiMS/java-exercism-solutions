7092f7a2fd2daa926756c2bd10c0942027b655c8
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 18:24:21 +0300

    micro-blog alternative solution

--- micro-blog/src/main/java/MicroBlog.java
+++ micro-blog/src/main/java/MicroBlog.java
@@ -1,7 +1,8 @@
 class MicroBlog {
     public String truncate(String input) {
-        return input.codePoints().limit(5)
-                .mapToObj(i -> new String((Character.toChars(i))))
-                .reduce("", String::concat);
+        StringBuffer buffer = new StringBuffer();
+        input.codePoints().limit(5)
+                .forEach(buffer::appendCodePoint);
+        return buffer.toString();
     }
 }
