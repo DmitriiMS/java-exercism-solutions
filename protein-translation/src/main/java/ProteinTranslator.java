4e6c5b9655dbfff668b25717369c4edb8fd6263b
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 21:14:58 +0300

    protein-translation solution

--- protein-translation/src/main/java/ProteinTranslator.java
+++ protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,39 @@
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class ProteinTranslator {
+    private final Map<String, String> codonToProtein = new HashMap<>(){{
+        put("AUG", "Methionine");
+        put("UUU", "Phenylalanine");
+        put("UUC", "Phenylalanine");
+        put("UUA", "Leucine");
+        put("UUG", "Leucine");
+        put("UCU", "Serine");
+        put("UCC", "Serine");
+        put("UCA", "Serine");
+        put("UCG", "Serine");
+        put("UAU", "Tyrosine");
+        put("UAC", "Tyrosine");
+        put("UGU", "Cysteine");
+        put("UGC", "Cysteine");
+        put("UGG", "Tryptophan");
+        put("UAA", "");
+        put("UAG", "");
+        put("UGA", "");
+    }};
 
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return splitRnaSeq(rnaSequence).stream()
+                .takeWhile(c -> !codonToProtein.get(c).isEmpty())
+                .map(codonToProtein::get)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> splitRnaSeq(String rnaSequence) {
+        List<String> codonList = new ArrayList<>();
+        for(int i = 0; i < rnaSequence.length(); i += 3) {
+            codonList.add(rnaSequence.substring(i, i + 3));
+        }
+        return codonList;
     }
 }
