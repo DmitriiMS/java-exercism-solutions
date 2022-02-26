5fc68537a5c77c9982545319f3df19750eb0cde8
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-26 17:21:08 +0300

    resistor-color-duo solution

--- resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class ResistorColorDuo {
+    private final Map<String, Integer> colorCodes = new HashMap<>();
+
+    public ResistorColorDuo() {
+        String[] colors = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        for(int i = 0; i < colors.length; i++){
+            colorCodes.put(colors[i], i);
+        }
+    }
+
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.stream(colors).limit(2).mapToInt(colorCodes::get).reduce(0, (a, b) -> a * 10 + b);
     }
 }
