9bdb0d95799085dd05d51a75a3859cbf7b78d810
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-03-07 20:18:41 +0300

    matrix solution

--- matrix/src/main/java/Matrix.java
+++ matrix/src/main/java/Matrix.java
@@ -1,15 +1,32 @@
+import java.util.Arrays;
 
 class Matrix {
 
-    Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private int[][] matrix;
+
+    public Matrix(String matrixAsString) {
+String[] rows = matrixAsString.split("\n");
+        int rowLength = rows[0].split(" ").length;
+        matrix = new int[rows.length][];
+        for(int i = 0; i < rows.length; i++){
+            if(rows[i].split(" ").length != rowLength) {
+                throw new IllegalArgumentException("Strange row: aborting!");
+            }
+            matrix[i] = Arrays.stream(rows[i].split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
     }
 
-    int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int[] getRow(int rowNumber) {
+        if(rowNumber < 1 || rowNumber > matrix.length) {
+            return null;
+        }
+        return matrix[rowNumber - 1];
     }
 
-    int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int[] getColumn(int columnNumber) {
+        if(columnNumber < 1 || columnNumber > matrix[0].length) {
+            return null;
+        }
+        return Arrays.stream(matrix).mapToInt(r -> r[columnNumber - 1]).toArray();
     }
 }
