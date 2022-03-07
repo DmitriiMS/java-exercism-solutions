f8c9aeff5e664b5c73b076ab1d5e543d04b32555
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-03-07 17:13:31 +0300

    perfect-numbers solution

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,44 @@
 class NaturalNumber {
 
+    private int number;
+    private int aliquotSum;
 
+    public NaturalNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        this.number = number;
+    }
 
+    public Classification getClassification() {
+        if(isPrime(number)){
+            return Classification.DEFICIENT;
+        }
+        calculateAliquotSum();
+        if(aliquotSum > number) {
+            return Classification.ABUNDANT;
+        }else if (aliquotSum < number) {
+            return Classification.DEFICIENT;
+        }else {
+            return Classification.PERFECT;
+        }
+    }
+
+    private void calculateAliquotSum() {
+        aliquotSum++;
+        for(int i = 2; i < Math.sqrt(number); i++) {
+            if(number % i == 0) {
+                aliquotSum += i + number/i;
+            }
+        }
+    }
+
+    private boolean isPrime(int num){
+        if(num == 1 || num == 2) { return true; }
+        if(num % 2 == 0) { return false;}
+        for(int i = 3; i < Math.sqrt(num); i += 2) {
+            if(num % i == 0) { return false; }
+        }
+        return true;
+    }
 }
