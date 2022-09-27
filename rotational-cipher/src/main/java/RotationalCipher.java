00444f4e2a901a01d9cd6d1d3ea22feaf1b542f9
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-09-27 12:15:11 +0300

    rotational cipher solution

--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,40 @@
 class RotationalCipher {
 
+    private int shiftKey;
+
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.shiftKey = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] rotated = new char[data.length()];
+        for(int i = 0; i < data.length(); i++) {
+            char c = data.charAt(i);
+            if(c > 64 & c < 91) {
+                c = rotateUppercase(c);
+            }
+            if(c > 96 & c < 123) {
+                c = rotateLowercase(c);
+            }
+            rotated[i] = c;
+        }
+        return new String(rotated);
+    }
+
+    char rotateLowercase(char c) {
+        c += shiftKey;
+        if (c > 122) {
+            c = (char) (96 + (c - 122));
+        }
+        return c;
+    }
+
+    char rotateUppercase(char c) {
+        c += shiftKey;
+        if (c > 90) {
+            c = (char) (64 + (c - 90));
+        }
+        return c;
     }
 
 }
