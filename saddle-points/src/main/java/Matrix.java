73cc6cf7c6c1dfb51ec1d0020276defcd0b6f7ce
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-10-02 11:14:54 +0300

    saddle points dumb solution

--- saddle-points/src/main/java/Matrix.java
+++ saddle-points/src/main/java/Matrix.java
@@ -1,13 +1,44 @@
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 class Matrix {
 
+    private List<List<Integer>> matrix;
+    private int rowLength;
+    private  Set<MatrixCoordinate> saddlePoints = new HashSet<>();
+
     Matrix(List<List<Integer>> values) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.matrix = values;
+        rowLength = values.isEmpty() ? 0 : values.get(0).size();
     }
 
     Set<MatrixCoordinate> getSaddlePoints() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (rowLength == 0) {
+            return saddlePoints;
+        }
+        int maxInARow;
+        int minInACol;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            maxInARow = matrix.get(i).stream().max(Integer::compareTo).get();
+            for (int j = 0; j < rowLength; j ++) {
+               if (matrix.get(i).get(j) == maxInARow) {
+                   minInACol = getColumn(j).stream().min(Integer::compareTo).get();
+                   if (maxInARow == minInACol) {
+                       saddlePoints.add(new MatrixCoordinate(i + 1, j + 1));
+                   }
+               }
+            }
+        }
+
+        return saddlePoints;
     }
+
+    private List<Integer> getColumn(int colIndex) {
+        List<Integer> column = new ArrayList<>();
+        for (int i = 0; i < matrix.size(); i++) {
+            column.add(matrix.get(i).get(colIndex));
+        }
+        return column;
+    }
+
 }
